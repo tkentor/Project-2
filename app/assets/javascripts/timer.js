5385a546a2ca676dc4d3f2c2d15a35caeb245a6a
Teddy Kentor <tkentor@gmail.com>
2015-11-19 12:13:43 -0500

    timer works just not very well

---
+++ app/assets/javascripts/timer.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  console.log("working")
+
+  var delay = null;
+
+    $(".time").change(function(){
+      delay = parseFloat($(this).val() ) * 60;
+      clearInterval(delay);
+      console.log("New Interval: " + delay);
+      }
+    )
+
+    $("#start").click(function() {
+      timeout = setTimeout(function() {
+        playSound('sound.wav')
+        alert('Finished');
+      }, delay)
+    })
+
+
+        // $("#stop").click(function() {
+        //       if (timeout) {
+        //           clearTimeout(timeout);
+        //           timeout = null;
+        //       }
+        //       alert('Stopped');
+        //   })
+
+
+
+    })
